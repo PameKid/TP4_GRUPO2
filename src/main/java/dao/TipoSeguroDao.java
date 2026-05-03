6928101d48e6786dd326fd4ed5cdab7fe30a714f
SebastianDte <sebateunmate@outlook.com>
2026-05-03 13:23:57 -0300

    tAREA 4.4

--- src/main/java/dao/TipoSeguroDao.java
+++ src/main/java/dao/TipoSeguroDao.java
@@ -1,5 +1,38 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import entidades.TipoSeguro;
+
 public class TipoSeguroDao {
 
+    public List<TipoSeguro> listarTiposSeguros() {
+        List<TipoSeguro> tipos = new ArrayList<TipoSeguro>();
+        String sql = "SELECT idTipo, descripcion FROM tipoSeguros";
+
+        try {
+            Connection conexion = Conexion.getInstancia().getConnection();
+            PreparedStatement statement = conexion.prepareStatement(sql);
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                int idTipo = resultSet.getInt("idTipo");
+                String descripcion = resultSet.getString("descripcion");
+                TipoSeguro tipo = new TipoSeguro(idTipo, descripcion);
+                tipos.add(tipo);
+            }
+
+            resultSet.close();
+            statement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return tipos;
+    }
 }
