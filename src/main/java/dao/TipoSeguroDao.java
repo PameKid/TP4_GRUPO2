d154b123aa9225167232e3b801fa872905b420ee
FloresSebastian23 <florescsebastian.23@gmail.com>
2026-05-04 01:14:26 -0300

    Agregado de listado seguro, y listado por tipo

--- src/main/java/dao/TipoSeguroDao.java
+++ src/main/java/dao/TipoSeguroDao.java
@@ -1,5 +1,6 @@
 package dao;
 
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -7,6 +8,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import entidades.Seguro;
 import entidades.TipoSeguro;
 
 public class TipoSeguroDao {
@@ -34,5 +36,87 @@ public class TipoSeguroDao {
         }
 
         return tipos;
+    }
+    
+    public List<Seguro> listarSeguros() throws Exception{
+        List<Seguro> lista = new ArrayList<>();
+        String sql = "SELECT s.idSeguro, s.descripcion, s.idTipo, s.costoContratacion, s.costoAsegurado, " +
+                     "t.descripcion AS tipoDescripcion " +
+                     "FROM seguros s " +
+                     "INNER JOIN tipoSeguros t ON s.idTipo = t.idTipo";
+
+        PreparedStatement pst = null;
+        ResultSet rs = null;
+
+        try {
+            pst = Conexion.getInstancia().getConnection().prepareStatement(sql);
+            rs = pst.executeQuery();
+
+            while (rs.next()) {
+                Seguro seguro = new Seguro();
+
+                seguro.setIdSeguro(rs.getInt("idSeguro"));
+                seguro.setDescripcion(rs.getString("descripcion"));
+
+                TipoSeguro tipo = new TipoSeguro();
+                tipo.setIdTipoSeguro(rs.getInt("idTipo"));
+                tipo.setDescripcionTipoSeguro(rs.getString("tipoDescripcion"));
+
+                seguro.setTipoSeguro(tipo);
+                seguro.setCostoContratacion(rs.getDouble("costoContratacion"));
+                seguro.setCostoMaximoAsegurado(rs.getDouble("costoAsegurado"));
+
+                lista.add(seguro);
+            }
+
+        } finally {
+            if (rs != null) rs.close();
+            if (pst != null) pst.close();
+        }
+
+        return lista;
+    }
+    
+    public List<Seguro> listarSegurosPorTipo(int idTipo) throws Exception {
+        List<Seguro> lista = new ArrayList<>();
+
+        String sql = "SELECT s.idSeguro, s.descripcion, s.idTipo, s.costoContratacion, s.costoAsegurado, " +
+                     "t.descripcion AS tipoDescripcion " +
+                     "FROM seguros s " +
+                     "INNER JOIN tipoSeguros t ON s.idTipo = t.idTipo " +
+                     "WHERE s.idTipo = ?";
+
+        PreparedStatement pst = null;
+        ResultSet rs = null;
+
+        try {
+            pst = Conexion.getInstancia().getConnection().prepareStatement(sql);
+            pst.setInt(1, idTipo);
+
+            rs = pst.executeQuery();
+
+            while (rs.next()) {
+                Seguro seguro = new Seguro();
+
+                seguro.setIdSeguro(rs.getInt("idSeguro"));
+                seguro.setDescripcion(rs.getString("descripcion"));
+
+                TipoSeguro tipo = new TipoSeguro();
+                tipo.setIdTipoSeguro(rs.getInt("idTipo"));
+                tipo.setDescripcionTipoSeguro(rs.getString("tipoDescripcion"));
+
+                seguro.setTipoSeguro(tipo);
+                seguro.setCostoContratacion(rs.getDouble("costoContratacion"));
+                seguro.setCostoMaximoAsegurado(rs.getDouble("costoAsegurado"));
+
+                lista.add(seguro);
+            }
+
+        } finally {
+            if (rs != null) rs.close();
+            if (pst != null) pst.close();
+        }
+
+        return lista;
     }
 }
